8a6e3256f4ceb1d8bcedc628c7dec9cb364b1fec
sania8 <108281312+sania8@users.noreply.github.com>
2023-07-21 23:14:10 +0530

    initial commit

--- second.java
+++ second.java
@@ -1,3 +1,14 @@
-public class second {
-    
+import java.util.Scanner;
+public class second{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number:");
+        int b = sc.nextInt();
+        System.out.println(a+"+"+b);
+        int c = a+b;
+        System.out.print(c);
+
+    }
 }
